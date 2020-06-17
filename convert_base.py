688dda906216da1093535e0d30ff2a0be11206f6
Du <1034331038@qq.com>
2020-06-18 01:35:08 +0800

    Create convert_base.py

---
+++ convert_base.py
@@ -0,0 +1,20 @@
+from stack_practice import Stack
+
+
+def baseConverter(decNum, base):
+    digits = "0123456789ABCDEF"
+    s = Stack()
+    while decNum > 0:
+        rem = decNum % base
+        s.push(rem)
+        decNum //= base
+    ans = ''
+    while not s.isEmpty():
+        ans += digits[s.pop()]
+    return ans
+
+
+if __name__ == '__main__':
+    print(baseConverter(15, 16))
+    print(baseConverter(15278, 16))
+    print(baseConverter(1024, 2))
