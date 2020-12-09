969d526d2fd83253ddba21aaf907ce4934ec92ba
Du <1034331038@qq.com>
2020-12-09 23:18:26 +0800

    test process img

--- img_process/img_processor.py
+++ img_process/img_processor.py
@@ -8,11 +8,17 @@ def testProcessImg():
     img.show()
     newImg = img.resize((x, y), Image.NEAREST)
     newImg.show()
-    newImg.save('../imgs/resize.jpg','JPEG',qulity=1)
-    nail = img.thumbnail((x, y))
+    newImg.save('../imgs/resize.jpg', 'JPEG', qulity=1)
+    nail = img.thumbnail()
     img.show()
-    img.save('../imgs/nail.jpg','JPEG',quality=20)
+    img.save('../imgs/nail.jpg', 'JPEG', quality=20)
+
+
+def testProcessImg2():
+    img = Image.open('../imgs/mainpic.jpg')
+    img.thumbnail((img.size))
+    img.save('../imgs/nail2.jpg', 'JPEG', quality=20)
 
 
 if __name__ == '__main__':
-    testProcessImg()
+    testProcessImg2()
